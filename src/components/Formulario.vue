84a184af0ce45ebdc840f6bf4f6f207bf4fc747b
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-06-23 16:46:05 -0300

    Commit 23062023-1645

---
+++ src/components/Formulario.vue
@@ -0,0 +1,20 @@
+<template>
+    <form @submit.prevent="cadastraTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required
+                    type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-1">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="evento => estado.filtro = evento.target.value" class="form-control">
+                    <option value="todas">Todas as tarefas</option>
+                    <option value="pendentes">Tarefas Pendentes</option>
+                    <option value="finalizadas">Tarefas Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
---
+++ src/components/List.vue
@@ -0,0 +1,12 @@
+<template>
+    {{ estado.filtro }}
+    <ul class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                :id="tarefa.titulo" type="checkbox">
+            <label :for="tarefa.titulo" :class="{ done: tarefa.finalizada }" class="ms-3">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+</template>
